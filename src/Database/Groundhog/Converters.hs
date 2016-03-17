3839b8bdb289255e8d73f2ca8d7697a080e1eda4
Scott Murphy <scottmurphy09@gmail.com>
2016-03-17 17:52:41 -0500

    Writing converter testsers

--- src/Database/Groundhog/Converters.hs
+++ src/Database/Groundhog/Converters.hs
@@ -14,6 +14,16 @@ import qualified  Data.Map.Strict  as Map
 import qualified Data.ByteString.Lazy as BL
 import           Data.Bimap                (Bimap)
 import qualified Data.Bimap                as Bimap
+--------------------------------------------------
+-- Imports TO TEST SUITE
+--------------------------------------------------
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+import Test.Tasty.Golden
+import Database.Groundhog.TH
+import Database.Groundhog.Core
+import Database.Groundhog.Sqlite
 
 -- | The type of a converter from a newtype or opaque type to a DB-serializable type
 type Converter a b = (a -> b, b -> a)
@@ -31,7 +41,43 @@ makeConverter :: (a -> b) -- ^ Convert developer-facing type to database-storabl
               -> (b -> a) -- ^ Convert database-storable type to developer-facing type
               -> Converter a b
 makeConverter ab ba = (ab, ba)
+{- TO TEST SUITE-}
 
+-- | Test round trip property of a converter,
+-- >>> roundTripConverter "should convert a RecordBiMap to a StorableList" (==) myRecordConverter
+roundTripConverter :: Arbitrary a => TestName -> (a -> a -> Bool) -> (Converter a b) -> TestTree
+roundTripConverter testName toBool converterToTest = testProperty testName  runRoundTripTest 
+  where
+    (f,g) = converterToTest
+    runRoundTripTest = do
+       val <- arbitrary
+       (return . toBool val . g . f) val
+
+-- | goldenFileConverter takes advantage of the file nature of SQLlite to read in your data
+-- using groundhog.  Even if you store your data in a different database
+-- if the serialization of a converter works in SQLite it will probably work in
+-- your given DB.  Obviously that isn't perfect but this makes writing a quick test easy
+
+goldenFileConverter :: TestName ->
+                         FilePath -> a -> 
+                         (b -> b -> Bool) -> Converter a b  ->
+                         TestTree
+goldenFileConverter testName someA fp aToBool converter  = _
+  where
+     (toB,toA) = converter
+     runSqlTest = withSqliteConn fp $ runDbConn $ do
+       runMigration $ do 
+             migrate (undefined :: a)
+       bs <- selectAll
+       case bs of
+         [] -> insert (toB someA) 
+         _ -> return $ and $ (\b -> ((== b) . toB.toA) b) <$> bs
+
+
+
+
+
+--------------------------------------------------
 -- | Reverse the direction of a converter
 flipConverter :: Converter a b -> Converter b a
 flipConverter (ba, ab) = (ab, ba)
@@ -71,3 +117,5 @@ bimapConverter = makeConverter Bimap.toList Bimap.fromList
 -- | Convert an 'IntMap' to a list of 'Int'-value pairs
 intMapConverter :: Converter (IntMap a) [(Int, a)]
 intMapConverter = makeConverter IntMap.toList IntMap.fromList
+
+
