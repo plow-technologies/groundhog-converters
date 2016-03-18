30ff8a29548df5b270db2966c8ff04e2a1322a81
Scott Murphy <scottmurphy09@gmail.com>
2016-03-18 14:32:13 -0500

    added a few ignores and updated cabal

--- src/Database/Groundhog/Converters.hs
+++ src/Database/Groundhog/Converters.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs             #-}
 {-# LANGUAGE TypeFamilies      #-}
-
+{-# LANGUAGE QuasiQuotes #-}
 module Database.Groundhog.Converters where
-import           Control.Arrow             (first, second)
+import           Control.Arrow             (first, second, (***))
 import Data.Aeson (ToJSON,FromJSON,encode,eitherDecode)
 import Data.Int (Int64)
 import   Data.Map.Strict (Map)
@@ -14,16 +15,6 @@ import qualified  Data.Map.Strict  as Map
 import qualified Data.ByteString.Lazy as BL
 import           Data.Bimap                (Bimap)
 import qualified Data.Bimap                as Bimap
---------------------------------------------------
--- Imports TO TEST SUITE
---------------------------------------------------
-import Test.Tasty
-import Test.Tasty.HUnit
-import Test.Tasty.QuickCheck
-import Test.Tasty.Golden
-import Database.Groundhog.TH
-import Database.Groundhog.Core
-import Database.Groundhog.Sqlite
 
 -- | The type of a converter from a newtype or opaque type to a DB-serializable type
 type Converter a b = (a -> b, b -> a)
@@ -41,43 +32,7 @@ makeConverter :: (a -> b) -- ^ Convert developer-facing type to database-storabl
               -> (b -> a) -- ^ Convert database-storable type to developer-facing type
               -> Converter a b
 makeConverter ab ba = (ab, ba)
-{- TO TEST SUITE-}
 
--- | Test round trip property of a converter,
--- >>> roundTripConverter "should convert a RecordBiMap to a StorableList" (==) myRecordConverter
-roundTripConverter :: Arbitrary a => TestName -> (a -> a -> Bool) -> (Converter a b) -> TestTree
-roundTripConverter testName toBool converterToTest = testProperty testName  runRoundTripTest 
-  where
-    (f,g) = converterToTest
-    runRoundTripTest = do
-       val <- arbitrary
-       (return . toBool val . g . f) val
-
--- | goldenFileConverter takes advantage of the file nature of SQLlite to read in your data
--- using groundhog.  Even if you store your data in a different database
--- if the serialization of a converter works in SQLite it will probably work in
--- your given DB.  Obviously that isn't perfect but this makes writing a quick test easy
-
-goldenFileConverter :: TestName ->
-                         FilePath -> a -> 
-                         (b -> b -> Bool) -> Converter a b  ->
-                         TestTree
-goldenFileConverter testName someA fp aToBool converter  = _
-  where
-     (toB,toA) = converter
-     runSqlTest = withSqliteConn fp $ runDbConn $ do
-       runMigration $ do 
-             migrate (undefined :: a)
-       bs <- selectAll
-       case bs of
-         [] -> insert (toB someA) 
-         _ -> return $ and $ (\b -> ((== b) . toB.toA) b) <$> bs
-
-
-
-
-
---------------------------------------------------
 -- | Reverse the direction of a converter
 flipConverter :: Converter a b -> Converter b a
 flipConverter (ba, ab) = (ab, ba)
@@ -86,9 +41,16 @@ flipConverter (ba, ab) = (ab, ba)
 composeConverter :: Converter a b -> Converter b c -> Converter a c
 composeConverter (ab, ba) (bc, cb) = (bc . ab, ba . cb)
 
+
+
 -- | Map a converter over a functor
 fmapConverter :: Functor f => Converter a b -> Converter (f a) (f b)
 fmapConverter (ba, ab) = (fmap ba, fmap ab)
+
+
+-- | compose a First and Second Converter 
+bicomposeConverter  :: Converter a b -> Converter c d -> Converter (a,c) (b,d)
+bicomposeConverter (ba,ab) (dc,cd) = (ba *** dc, ab *** cd)
 
 -- | Convert only the first element of a pair
 firstConverter :: Converter a b -> Converter (a, c) (b, c)
@@ -117,5 +79,4 @@ bimapConverter = makeConverter Bimap.toList Bimap.fromList
 -- | Convert an 'IntMap' to a list of 'Int'-value pairs
 intMapConverter :: Converter (IntMap a) [(Int, a)]
 intMapConverter = makeConverter IntMap.toList IntMap.fromList
-
 
